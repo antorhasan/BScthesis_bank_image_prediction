0c75a57973aca612d25f1e4ea3e626836333b097
antorhasan <kaziantorhasan@gmail.com>
2018-11-18 16:56:11 +0600

    nothing

--- first.py
+++ first.py
@@ -15,3 +15,17 @@ root_logdir = "tf_logs"
 logdir = "{}/run-{}/".format(root_logdir, now)
 root_logdir_m = "tf_models"
 logdir_m = "{}/run-{}/".format(root_logdir_m, now)
+
+def _parse_function(example_proto):
+    features = {
+                "image_y": tf.FixedLenFeature((), tf.string )
+                }
+
+    parsed_features = tf.parse_single_example(example_proto, features)
+
+    image_y = tf.decode_raw(parsed_features["image_y"],  tf.float64)
+
+    image_y = tf.reshape(image_y, [256,256,1])
+    image_y = tf.cast(image_y,dtype=tf.float32)
+
+    return image_y
