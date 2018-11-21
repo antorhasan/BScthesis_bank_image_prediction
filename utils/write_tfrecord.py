2802d5278522e50c3d8eaa8f1d751ee0823975ff
antorhasan <kaziantorhasan@gmail.com>
2018-11-22 00:34:24 +0600

    all tfrecord files created

--- utils/write_tfrecord.py
+++ utils/write_tfrecord.py
@@ -13,7 +13,7 @@ def _bytes_feature(value):
 def _int64_feature(value):
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
-trainY = "/media/antor/Files/main_projects/rnn_test/*.jpg"
+trainY = "/media/antor/Files/main_projects/test_set/*.jpg"
 trainY_list = glob.glob(trainY)
 only = [os.path.basename(x) for x in trainY_list]
 onl = [os.path.splitext(x)[0] for x in only]
@@ -21,21 +21,19 @@ onl = [os.path.splitext(x)[0] for x in only]
 results = list(map(int, onl))
 results.sort()
 resul = list(map(str, results))
+#resul = resul[0:3]
 print(resul)
 
 
-writer = tf.python_io.TFRecordWriter("/media/antor/Files/ML/Papers/train_rnn_test.tfrecords")
+writer = tf.python_io.TFRecordWriter("/media/antor/Files/ML/tfrecord/sir_demo/test.tfrecords")
 for i in range(len(resul)):
     print(i)
-    img_y = cv2.imread("/media/antor/Files/main_projects/finally/"+resul[i]+".jpg", cv2.IMREAD_GRAYSCALE)
-    #img_y = cv2.fastNlMeansDenoising(img_y, h=24, templateWindowSize=7, searchWindowSize=21)
-
+    img_y = cv2.imread("/media/antor/Files/main_projects/test_set/"+resul[i]+".jpg", cv2.IMREAD_GRAYSCALE)
+    img_y = cv2.fastNlMeansDenoising(img_y, h=24, templateWindowSize=7, searchWindowSize=21)
     img_y = np.asarray(img_y)
     #img_y = np.asarray(cv2.imread(addrs_y[i], cv2.IMREAD_GRAYSCALE))
-
     last_y = np.reshape(img_y, (256,256,1))
     last_y = last_y/255
-
     feature = {
         'image_y': _bytes_feature(last_y.tostring())
         #'image_x': _bytes_feature(last_x.tostring())
